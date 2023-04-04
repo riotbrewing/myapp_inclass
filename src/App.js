493b73f0945f91f073d53b18b72490cfe69b0e93
Andrew Steele <aesteele@dmacc.edu>
2023-04-03 19:17:49 -0500

    added some state and changed the onChange event

--- src/App.js
+++ src/App.js
@@ -1,41 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+
+//const App = () => {}
 
 function App() {
-  let todoListItems =[
-    "Laundry",
-    "Dishes",
-    "Sweep the floor",
-    "Feed the cats",
-    "Clean the shower",
-    "Random task A",
-    "Another task"
-  ];
-  return (
-      <div>
-        <ul>
-          {
-            todoListItems.map((item, index) => {
-              return (<li key = {item + index}>{item}</li>)
-            })
-          }
-        </ul>
-          {/* events must be camel cased */}
+    const [listItems, setListItem] = React.useState([])
 
-        <input
-            onMouseOver={(event) =>
-                {
-                    console.log("Moused Over")
-                }}
+    //this is basically equivalent to this...
+    /*
+        const useState = () => {
+        //logic goes here
 
-            onChange= {(event) =>
-                {
-                    console.log(event.target.value)
-                }
+        return [yourVariable, setYourVariable]
         }
-        />
-      </div>
-  );
-}
+     */
+
+     /*
+        let todoListItems =[
+        "Laundry",
+        "Dishes",
+        "Sweep the floor",
+        "Feed the cats",
+        "Clean the shower",
+        "Random task A",
+        "Another task"
+          ];
+      */
+      return (
+          <div>
+            <ul>
+              {
+                  listItems.map((item, index) => {
+                  return (<li key = {item + index}>{item}</li>)
+                })
+              }
+            </ul>
+              {/* events must be camel cased */}
+
+            <input
+                onMouseOver={(event) =>
+                    {
+                        console.log("Moused Over")
+                    }}
+
+                onChange= {(event) =>
+                    {
+                        setListItem([event.target.value])
+                        console.log(event.target.value)
+                    }
+            }
+            />
+          </div>
+      );
+    }
 
 export default App;
